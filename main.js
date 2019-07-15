a9769d823e23d99707882212a6ee97b3e79df039
Alex Faissal <alefaissal@gmail.com>
2019-07-14 17:22:15 -0700

    Add cart numbers

--- main.js
+++ main.js
@@ -23,3 +23,67 @@ subscribe.addEventListener("click", function (event) {
     alert("Thank you for subscribe");
   }
 });
+
+// Shopping cart  
+const cartCount = document.getElementById("shopping-number");
+
+const productCart1 = document.getElementById("product1-cart");
+const productCart2 = document.getElementById("product2-cart");
+const productCart3 = document.getElementById("product3-cart");
+const productCart4 = document.getElementById("product4-cart");
+const productCart5 = document.getElementById("product5-cart");
+const productCart6 = document.getElementById("product6-cart");
+const productCart7 = document.getElementById("product7-cart");
+const productCart8 = document.getElementById("product8-cart");
+
+let click = 0;
+
+productCart1.addEventListener("click", function (event){
+  console.log("adding to shopping cart");
+  click++;
+  cartCount.innerHTML = click;
+  cartCount.style.backgroundColor = "red";
+});
+productCart2.addEventListener("click", function (event){
+  console.log("adding to shopping cart");
+  click++;
+  cartCount.innerHTML = click;
+  cartCount.style.backgroundColor = "red";
+});
+productCart3.addEventListener("click", function (event){
+  console.log("adding to shopping cart");
+  click++;
+  cartCount.innerHTML = click;
+  cartCount.style.backgroundColor = "red";
+});
+productCart4.addEventListener("click", function (event){
+  console.log("adding to shopping cart");
+  click++;
+  cartCount.innerHTML = click;
+  cartCount.style.backgroundColor = "red";
+});
+productCart5.addEventListener("click", function (event){
+  console.log("adding to shopping cart");
+  click++;
+  cartCount.innerHTML = click;
+  cartCount.style.backgroundColor = "red";
+});
+productCart6.addEventListener("click", function (event){
+  console.log("adding to shopping cart");
+  click++;
+  cartCount.innerHTML = click;
+  cartCount.style.backgroundColor = "red";
+});
+productCart7.addEventListener("click", function (event){
+  console.log("adding to shopping cart");
+  click++;
+  cartCount.innerHTML = click;
+  cartCount.style.backgroundColor = "red";
+});
+productCart8.addEventListener("click", function (event){
+  console.log("adding to shopping cart");
+  click++;
+  cartCount.innerHTML = click;
+  cartCount.style.backgroundColor = "red";
+});
+
